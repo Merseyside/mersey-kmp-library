d5a8525659827e4e30d918146f34c4ab946ef5fa
Merseyside <ivanklessablin@gmail.com>
2021-09-21 18:56:24 +0700

    Make di receive additional params

--- archy-android/src/main/java/com/merseyside/merseyLib/archy/android/presentation/di/KoinScopedExt.kt
+++ archy-android/src/main/java/com/merseyside/merseyLib/archy/android/presentation/di/KoinScopedExt.kt
@@ -6,21 +6,24 @@ import com.merseyside.merseyLib.archy.core.presentation.model.StateViewModel.Com
 import com.merseyside.merseyLib.utils.core.SavedState
 import com.merseyside.utils.ext.getSerialize
 import com.merseyside.utils.ext.isNotNullAndEmpty
+import com.merseyside.utils.ext.log
 import kotlinx.serialization.builtins.MapSerializer
 import kotlinx.serialization.builtins.serializer
+import org.koin.core.definition.Definition
 import org.koin.core.instance.InstanceFactory
 import org.koin.core.module.Module
 import org.koin.core.qualifier.Qualifier
-import org.koin.core.scope.Scope
 import org.koin.dsl.ScopeDSL
 
 inline fun <reified T : StateViewModel> ScopeDSL.stateViewModel(
     qualifier: Qualifier? = null,
-    noinline viewModelDefinition: Scope.() -> T
+    noinline viewModelDefinition: Definition<T>
 ): Pair<Module, InstanceFactory<T>> {
 
-    return factory(qualifier) { (bundle: Bundle) ->
-        viewModelDefinition().apply {
+    return factory(qualifier) {
+        it.values.log(prefix = "kek")
+        viewModelDefinition(it).apply {
+            val bundle = it.getOrNull<Bundle>()
             if (bundle.isNotNullAndEmpty()) {
                 val savedState = SavedState().apply {
                     addAll(bundle.getSerialize(
