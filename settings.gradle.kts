cd3cc46bab04168cda210d56a229d382b5eceeda
Vladimir <v.mudryakov@dev.izibook.ru>
2021-12-16 13:48:57 +0300

    dialogs fixed, parcel fixed dont merge at this commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ dependencyResolutionManagement {
 
         val androidLibs by creating {
             from("$group:catalog-version-android:$catalogVersions")
-            version("merseyLib", "1.7.3")
+            version("merseyLib", "1.7.4")
         }
 
         val common by creating {
