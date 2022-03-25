c1ed5115efb30d009b8f152dd4dae8cc51ed07a7
Ivan Sablin <ivansablin@MacBook-Pro-Ivan.local>
2022-03-26 01:50:06 +0700

    Update android sdk versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,11 +1,9 @@
 object Application {
     const val groupId = "io.github.merseyside"
-    const val applicationId = "com.merseyside.merseyLib"
 
     const val version = Metadata.version
-    const val versionCode = 146
 
-    const val compileSdk = 30
-    const val targetSdk = 30
+    const val compileSdk = 31
+    const val targetSdk = 31
     const val minSdk = 21
 }
