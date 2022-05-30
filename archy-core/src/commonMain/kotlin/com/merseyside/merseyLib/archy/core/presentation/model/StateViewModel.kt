2241f20134a816e1b03c3992c8ddbd91587fb9fa
Andrey <portnyagin@dev.izibook.ru>
2022-05-30 17:50:17 +0400

    optional saved state in state viewmodel

--- archy-core/src/commonMain/kotlin/com/merseyside/merseyLib/archy/core/presentation/model/StateViewModel.kt
+++ archy-core/src/commonMain/kotlin/com/merseyside/merseyLib/archy/core/presentation/model/StateViewModel.kt
@@ -6,7 +6,7 @@ import com.merseyside.merseyLib.utils.core.state.StateSaver
 
 
 abstract class StateViewModel(
-    override val savedState: SavedState = DummySavedState()
+    override val savedState: SavedState? = DummySavedState()
 ): BaseViewModel(), StateSaver {
 
     companion object {
