d305ccf0475e58d795b2aba8d38cd7e76eba5333
Ivan Sablin <ivansablin@MacBook-Pro-Ivan.local>
2022-12-28 18:37:23 +0600

    Add savedScopedOf funcs

--- utils-core/src/commonMain/kotlin/com/merseyside/merseyLib/utils/core/koin/scope/dsl/SavedScopedOf.kt
+++ utils-core/src/commonMain/kotlin/com/merseyside/merseyLib/utils/core/koin/scope/dsl/SavedScopedOf.kt
@@ -61,3 +61,19 @@ inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T
 inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10, reified T11, reified T12, reified T13> ScopeDSL.savedScopedOf(
     crossinline constructor: (SavedState, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13) -> R,
 ): KoinDefinition<R> where R : StateSaver = scoped { new(constructor) }
+
+inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10, reified T11, reified T12, reified T13, reified T14> ScopeDSL.savedScopedOf(
+    crossinline constructor: (SavedState, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14) -> R,
+): KoinDefinition<R> where R : StateSaver = scoped { new(constructor) }
+
+inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10, reified T11, reified T12, reified T13, reified T14, reified T15> ScopeDSL.savedScopedOf(
+    crossinline constructor: (SavedState, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15) -> R,
+): KoinDefinition<R> where R : StateSaver = scoped { new(constructor) }
+
+inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10, reified T11, reified T12, reified T13, reified T14, reified T15, reified T16> ScopeDSL.savedScopedOf(
+    crossinline constructor: (SavedState, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16) -> R,
+): KoinDefinition<R> where R : StateSaver = scoped { new(constructor) }
+
+inline fun <reified R, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10, reified T11, reified T12, reified T13, reified T14, reified T15, reified T16, reified T17> ScopeDSL.savedScopedOf(
+    crossinline constructor: (SavedState, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17) -> R,
+): KoinDefinition<R> where R : StateSaver = scoped { new(constructor) }
