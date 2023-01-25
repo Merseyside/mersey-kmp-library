0a2b92c0d9a59b54719c2944210358e01fc9a6ab
Ivan Sablin <ivansablin@MacBook-Pro-Ivan.local>
2023-01-25 19:25:34 +0600

    Up catalog version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ dependencyResolutionManagement {
         google()
     }
 
-    val catalogVersions = "1.6.4"
+    val catalogVersions = "1.6.6"
     val group = "io.github.merseyside"
     versionCatalogs {
         val multiplatformLibs by creating {
