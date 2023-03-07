dce2805e01393faec3cf93985b421d1273958662
Andrey Portnyagin <portnyagin@dev.izibook.ru>
2023-03-07 18:04:15 +0300

    added paging up

--- utils-core/src/commonMain/kotlin/com/merseyside/merseyLib/utils/core/pagination/Pagination.kt
+++ utils-core/src/commonMain/kotlin/com/merseyside/merseyLib/utils/core/pagination/Pagination.kt
@@ -2,30 +2,46 @@ package com.merseyside.merseyLib.utils.core.pagination
 
 import com.merseyside.merseyLib.kotlin.entity.Result
 import com.merseyside.merseyLib.kotlin.logger.ILogger
+import com.merseyside.merseyLib.kotlin.logger.log
 import com.merseyside.merseyLib.kotlin.utils.safeLet
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.map
 
-abstract class Pagination<PD, Data, Page>(private val initPage: Page): ILogger
+abstract class Pagination<PD, Data, Page>(
+    private val initNextPage: Page,
+    private val initPrevPage: Page
+) : ILogger
         where PD : PagerData<Data, Page> {
 
     private var lastData: PD? = null
-    var currentPage: Page = initPage
+    var currentNextPage: Page = initNextPage
+    var currentPrevPage: Page = initPrevPage
 
-    private val mutSharedFlow: MutableSharedFlow<Result<Data>> = MutableSharedFlow(extraBufferCapacity = 10)
+    private val pages = mutableMapOf<Page?, Page?>(initPrevPage to initNextPage)
+
+    private val mutSharedFlow: MutableSharedFlow<Result<Data>> =
+        MutableSharedFlow(extraBufferCapacity = 10)
     val resultFlow: Flow<Result<Data>> = mutSharedFlow
 
     val dataFlow: Flow<Data> = resultFlow.filterIsInstance<Result.Success<Data>>().map { it.value }
 
     private fun getNextPage(): Page {
         return safeLet(lastData) {
-            it.nextPage
-        } ?: initPage
+            pages[it.prevPage] = it.nextPage
+            findLastNextPage(it.nextPage ?: initPrevPage)
+        } ?: initNextPage
     }
 
-    abstract suspend fun loadData(page: Page): PD
+    private fun getPrevPage(): Page {
+        return safeLet(lastData) {
+            pages[it.prevPage] = it.nextPage
+            findFirstPrevPage(it.prevPage ?: initPrevPage)
+        } ?: initPrevPage
+    }
+
+    abstract suspend fun loadData(nextPage: Page, prevPage: Page): PD
 
     private suspend fun onDataLoaded(pagerData: PD) {
         lastData = pagerData
@@ -36,9 +52,14 @@ abstract class Pagination<PD, Data, Page>(private val initPage: Page): ILogger
         return getNextPage() != null || lastData == null
     }
 
+    private fun isPrevPageValid(): Boolean {
+        return getPrevPage() != null
+    }
+
     fun resetPaging() {
         lastData = null
-        currentPage = initPage
+        currentNextPage = initNextPage
+        currentPrevPage = initPrevPage
     }
 
     suspend fun loadNextPage() {
@@ -47,15 +68,46 @@ abstract class Pagination<PD, Data, Page>(private val initPage: Page): ILogger
             return
         }
 
-        currentPage = getNextPage()
+        currentNextPage = getNextPage()
 
         try {
-            val newData = loadData(currentPage)
+            val newData = loadData(currentNextPage, initPrevPage)
             emitResult(Result.Loading())
             onDataLoaded(newData)
         } catch (e: Exception) {
             emitResult(Result.Error(e))
         }
+    }
+
+    suspend fun loadPrevPage() {
+        if (!isPrevPageValid()) {
+            logMsg("No prev page")
+            return
+        }
+
+        currentPrevPage = getPrevPage()
+
+        try {
+            val newData = loadData(initNextPage, currentPrevPage)
+            emitResult(Result.Loading())
+            onDataLoaded(newData)
+        } catch (e: Exception) {
+            emitResult(Result.Error(e))
+        }
+    }
+
+    private fun findFirstPrevPage(page: Page): Page {
+        val prevPage = pages.entries.find { entry -> entry.value == page }?.key
+            ?: return page
+
+        return findFirstPrevPage(prevPage)
+    }
+
+    private fun findLastNextPage(page: Page): Page {
+        val nextPage = pages[page]
+            ?: return page
+
+        return findLastNextPage(nextPage)
     }
 
     private suspend fun emitResult(result: Result<Data>) {
